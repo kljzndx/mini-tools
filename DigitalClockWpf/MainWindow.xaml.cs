7a265ca867367e4277be7d5fe86fdac50ffd0083
李继龙 <kljzndx@outlook.com>
2023-10-22 07:58:28 +0800

    初始提交

---
+++ DigitalClockWpf/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+
+namespace DigitalClockWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Title = String.Empty;
+            this.Left = SystemParameters.WorkArea.Width - this.Width;
+            this.Top = SystemParameters.WorkArea.Height - this.Height;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += (s, e) =>
+            {
+                var now = DateTime.Now;
+                string longTimeString = now.ToLongTimeString();
+
+                Time_TextBlock.Text = longTimeString;
+
+                string filePath = Path.Combine(Environment.CurrentDirectory, "date-time.txt");
+                File.WriteAllText(filePath, $"{now.ToShortDateString()}    {longTimeString}", Encoding.UTF8);
+            };
+            timer.Start();
+        }
+    }
+}
