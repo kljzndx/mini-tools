8b79a571ef7887ec57b27c506c0f3963209dbc8b
李继龙 <kljzndx@outlook.com>
2024-01-03 13:34:02 +0800

    初始提交

---
+++ RenJiaoBookDownloader/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace RenJiaoBookDownloader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("按下回车键即可退出……");
+            Console.ReadLine();
+        }
+    }
+}
