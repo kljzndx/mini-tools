3e70eda2020e6033c14aed74b6e7b5f51a2d6493
李继龙 <kljzndx@outlook.com>
2024-01-03 13:36:33 +0800

    完成打开人教书籍官网功能

--- RenJiaoBookDownloader/Program.cs
+++ RenJiaoBookDownloader/Program.cs
@@ -6,6 +6,13 @@ namespace RenJiaoBookDownloader
     {
         static void Main(string[] args)
         {
+            Console.Title = "人教版教材下载器";
+            Console.WriteLine("人教版教材下载器 v1.0.0");
+            Console.Write("请输入书籍预览页面地址（留空则会打开书籍目录）：");
+            string address = Console.ReadLine() ?? string.Empty;
+            if (string.IsNullOrEmpty(address))
+                Process.Start(new ProcessStartInfo("https://jc.pep.com.cn/") { UseShellExecute = true });
+
             Console.WriteLine("按下回车键即可退出……");
             Console.ReadLine();
         }
